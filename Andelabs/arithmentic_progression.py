94a17c6d4be6a99bb172e1210676498e8ced4a92
Elizabeth Mabishi <emabishi@yahoo.com>
2016-04-29 01:40:48 +0300

    Progression

--- Andelabs/arithmentic_progression.py
+++ Andelabs/arithmentic_progression.py
@@ -7,4 +7,28 @@ For geometric progressions, return Geometric
 For neither of the above, return -1
 For an empty array, return 0
 '''
+'''
+def check_progression(x):
+	type(x) == list
+	count = 0
+	while len(x) >= 0:
+			return "Arithmetic"
+'''
 
+
+def is_arithmetic(l):
+
+    if len(l) == 0:
+        	return 0
+    #delta = l[1] - l[0]
+    for index in range(len(l) - 1): 	
+        if (l[index + 1] - l[index] == l[index + 2]  - l[index + 1]):
+        	return "Arithmetic" ##correct
+        elif (l[index + 1] / l[index]) == (l[index + 2] / l[index + 1]):
+        	return "Geometric"
+        else:
+        	return -1
+
+print is_arithmetic([])
+print is_arithmetic([2,4,6,8])
+print is_arithmetic([2,6,18,54,162])
--- Andelabs/test.py
+++ Andelabs/test.py
@@ -15,3 +15,11 @@ def arg_len(x,y,z):
 	print len(z)
 
 arg_len([1,2,3], [1,4,5], [1.2])
+
+
+def check_progression(x):
+	type(x) == array
+	while len(x) >= 0:
+		for y in x:
+			if x[len(x)] - x[len(x) - 1] == x[len(x) - 2] - x[len(x) - len(x)]:
+				return "Arithmetic"
